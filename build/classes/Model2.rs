ca860405bf4ae048388f8443bca2d3a693e581ef
HazemAhmedSaad <zozahmed939@gmail.com>
2023-01-21 01:07:15 +0200

    Upload the IMS project

---
+++ build/classes/Model2.rs
@@ -0,0 +1,1 @@
+model.pkg2.Model2
---
+++ src/Model2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.pkg2;
+
+/**
+ *
+ * @author HOSSAM
+ */
+public class Model2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
